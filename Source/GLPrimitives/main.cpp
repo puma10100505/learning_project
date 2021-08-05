60121ccc2cca6d718a984490da1a907136aa4702
billypu <puma_tw1@126.com>
2021-08-06 00:55:21 +0800

    mod

--- Source/GLPrimitives/main.cpp
+++ Source/GLPrimitives/main.cpp
@@ -13,7 +13,7 @@
 extern glm::vec4 BackgroundColor;
 extern float DeltaTime;
 
-static unsigned int VBO, VAO;
+static unsigned int VBO, VAO, EBO;
 
 std::unique_ptr<Shader> GlobalShader = nullptr;
 
@@ -26,8 +26,11 @@ static void OnKeyboardEvent(GLFWwindow* InWindow, int Key, int ScanCode, int Act
 }
 
 static const GLfloat LineVertices[] {
-    200, 100, 0,
-    100, 300, 0
+   -.8f, .8f, 0.f, .8f, -.8f, 10.f
+};
+
+static const unsigned int Indices[] = {
+    0, 1
 };
 
 static void OnTick(float DeltaTime)
@@ -35,13 +38,18 @@ static void OnTick(float DeltaTime)
     if (GlobalShader.get())
     {
         GlobalShader->use();
-        GlobalShader->setVec4("Color", glm::vec4(0.5f, sin(glfwGetTime()) / 2.f + 0.5f, 0.5f, 1.f));
+        GlobalShader->setVec4("Color", glm::vec4(.5f, .5f, .6f, 1.f));
+        //GlobalShader->setVec4("Color", glm::vec4(0.5f, sin(glfwGetTime()) / 2.f + 0.5f, 0.5f, 1.f));
     }
 
-    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // 先绑定VAO对象
+    glBindVertexArray(VAO); 
+    glDrawElements(GL_LINES, 2, GL_UNSIGNED_INT, 0);
+    // 渲染完成后再解绑VAO对象
+    glBindVertexArray(0);
 
-    printf("Get the last gl error: %x\n", glGetError());
+
+    //printf("Get the last gl error: %x\n", glGetError());
 }
 
 static void OnGUI(float DeltaTime)
@@ -77,9 +85,7 @@ int main(int argc, char** argv)
     }
 
     GlobalShader = std::make_unique<Shader>(
-        (solution_base_path + "Assets/Shaders/demo.vs").c_str(), 
-        (solution_base_path + "Assets/Shaders/demo.fs").c_str()
-    );
+        (solution_base_path + "Assets/Shaders/demo.vs").c_str(), (solution_base_path + "Assets/Shaders/demo.fs").c_str());
 
     float vertices[] = {
         -0.5f, -0.5f, 0.0f, // left  
@@ -87,25 +93,55 @@ int main(int argc, char** argv)
          0.0f,  0.5f, 0.0f  // top   
     }; 
 
+    // float LineVertices[] = {
+    //     -.8f, .8f, 0.f,
+    //     .8f, -.8f, 10.f
+    // };
     
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    // glGenVertexArrays(1, &VAO);
+    // glGenBuffers(1, &VBO);
+    // glBindVertexArray(VAO);
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // glEnableVertexAttribArray(0);
+    // glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    // glBindVertexArray(0); 
+
+    // -----------------
+
+    // glGenVertexArrays(1, &VAO);
+    // glGenBuffers(1, &VBO);
+    // glBindVertexArray(VAO);
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(LineVertices), LineVertices, GL_STATIC_DRAW);
+    // glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // glEnableVertexAttribArray(0);
+    // glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // glBindVertexArray(0);
+
+// 1. 将顶点数据存入到BUFFER中，但是还不知道如何解释这些数据
+    
+    glGenBuffers(1, &VBO);  // 生成顶点BUFFER
+    glGenBuffers(1, &EBO);  //  生成索引BUFFER 
+    glGenVertexArrays(1, &VAO); // 顶点数组
+    
+    // 先绑定VAO，在此之后绑定的VBO和EBO会自动记录在VAO中
     glBindVertexArray(VAO);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); // 绑定VBO到GL_ARRAY_BUFFER类型，指定VBO变量对应的BUFFER是VBO类型
+    glBufferData(GL_ARRAY_BUFFER, sizeof(LineVertices), LineVertices, GL_STATIC_DRAW); // 发送顶点数据
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 2. 解析BUFFER中的数据
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    // 因为之前已经绑定了VAO， 所以在这里绑定的EBO会记录在VAO中
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
 
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glEnableVertexAttribArray(0);       // 这个调用的参数对应Vertex Shader中 location=0的参数
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-    glBindVertexArray(0); 
-
+    
     GLWindowTick(OnTick, OnGUI);
 
     GLDestroyWindow();
