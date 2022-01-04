ba05ab222a37f19306e41f1f81974d9f2100f849
billypu <billypu@tencent.com>
2022-01-04 20:46:04 +0800

    add dx app

---
+++ Source/DxApp/DXSample.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <d3d12.h>
+#include <dxgidebug.h>
+
+using namespace std;
+using namespace DirectX;
+using namespace Microsoft::WRL;
+
+class D3DHelloTriangle: public DXSample 
+{
+    
+}
