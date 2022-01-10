8895e998b73296df2b4392a131e0c23703e3b125
billypu <billypu@tencent.com>
2022-01-11 02:00:45 +0800

    update

---
+++ Source/D3DMathMatrix/D3DMathMatrix.cpp
@@ -0,0 +1,26 @@
+#include <Windows.h>
+#include <string>
+#include <iostream>
+
+#include "CommonDefines.h"
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+
+#include "plog/Log.h"
+#include "plog/Initializers/RollingFileInitializer.h"
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR CmdLine, int nCmdShow)
+{
+    plog::init(plog::debug, (DefaultLogDirectory + "D3DMathMatrix.log").c_str());
+
+    if (XMVerifyCPUSupport() == false)
+    {
+        PLOGD << "CPU does not support SSE2";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
