98b24b452efa58fac2fcf424c79484a3512fefd7
billypu <billypu@tencent.com>
2022-01-10 12:53:23 +0800

    import bullet3 ogre and box2d

---
+++ Include/ogre/OgreConfigPaths.h
@@ -0,0 +1,8 @@
+#ifndef OGRECONFIGPATHS_H_
+#define OGRECONFIGPATHS_H_
+
+#define OGRE_CONFIG_DIR "D:/Development/Projects/ogre-13.2.4/Build/sdk/bin"
+#define OGRE_MEDIA_DIR "D:/Development/Projects/ogre-13.2.4/Build/sdk/Media"
+#define OGRE_PLUGIN_DIR "D:/Development/Projects/ogre-13.2.4/Build/sdk/bin" 
+
+#endif /* OGRECONFIGPATHS_H_ */
